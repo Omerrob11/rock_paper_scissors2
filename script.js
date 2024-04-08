6624ffaa008dc1d394935693c3833fa4db4b1077
Omer Miloh <omerrob11@gmail.com>
2024-04-08 14:15:09 +0300

    creating getCompuerChoice function

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function getComputerChoice(){
+const rock = "Rock";
+const paper = "Paper";
+const scissors = 'Scissors';
 
+function getComputerChoice(){
+    let numBetween1To3 = randomNum();
+
+    if (numBetween1To3 == 3) {
+        return rock;
+    } else if (numBetween1To3 == 2) {
+        return paper;
+    } else {
+        return scissors
+    }
 }
 
 function randomNum() {
