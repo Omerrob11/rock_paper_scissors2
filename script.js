4f13d911f49e0d06209e6a8a6ddd86c8ffc84137
Omer Miloh <omerrob11@gmail.com>
2024-04-09 13:01:10 +0300

    Adding playRound function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const rock = "Rock";
-const paper = "Paper";
-const scissors = 'Scissors';
+const rock = "rock";
+const paper = "paper";
+const scissors = 'scissors';
 
 function getComputerChoice(){
     let numBetween1To3 = randomNum();
@@ -16,4 +16,34 @@ function getComputerChoice(){
 
 function randomNum() {
     return Math.floor(Math.random() * 3) + 1;
+}
+
+function getPlayerSelection() {
+    return prompt("Pick a hand").toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection !== computerSelection) {
+        if (playerSelection == rock && computerSelection == scissors) {
+            return "player Won";
+        } else if(playerSelection === rock && computerSelection === paper) {
+            return "computer won";
+        } else if(playerSelection === paper && computerSelection === scissors) {
+            return "computer won";
+        } else if (playerSelection === paper && computerSelection === rock) {
+            return "player won";
+        } else if (playerSelection === scissors && computerSelection === rock) {
+            return "computer won";
+        } else { // this is where player selection equal scisors, and computer slection is paper 
+            return "player Won"
+        }
+    } else {
+        return "its a tie";
+    }
+
+}
+
+function playGame() {
+    
 }
