b73202cc6763c5c15d11de5e389a53da90609761
Omer Miloh <omerrob11@gmail.com>
2024-04-17 12:08:32 +0300

    changes

--- script.js
+++ script.js
@@ -26,24 +26,59 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection !== computerSelection) {
         if (playerSelection == rock && computerSelection == scissors) {
-            return "player Won";
+            return `You won! ${rock} beats ${scissors}`;
         } else if(playerSelection === rock && computerSelection === paper) {
-            return "computer won";
+            return`You lose! ${rock} lose to ${paper}`;
         } else if(playerSelection === paper && computerSelection === scissors) {
-            return "computer won";
+            return `You lose! ${paper} lose to ${scissors}`;
         } else if (playerSelection === paper && computerSelection === rock) {
-            return "player won";
+            return `You won! ${paper} beats ${rock}`;
         } else if (playerSelection === scissors && computerSelection === rock) {
-            return "computer won";
+            return `You lose! ${scissors} lose to ${rock}=`;
         } else { // this is where player selection equal scisors, and computer slection is paper 
-            return "player Won"
+            return `You won! ${scissors} beats ${paper}`;
         }
     } else {
-        return "its a tie";
+        return "its a tie round";
     }
-
 }
+
 
 function playGame() {
-    
+    let computerScore = 0;
+    let playerScore = 0;
+    for (let i = 0; i<=4; i++) {
+        let playerSelection = getPlayerSelection();
+        let computerSelection = getComputerChoice();
+        let roundWinner = playRound(playerSelection,computerSelection); 
+        if (calculateScore(roundWinner) === "player") {
+            playerScore += 1;
+        } else if(calculateScore(roundWinner) == "computer") {
+            computerScore +=1;
+        } else {
+            playerScore += 1;
+            computerScore +=1;
+        }
+
+    }
+
+    if ( computerScore > playerScore) {
+        return "compute won the game!"
+    } else if (playerScore > computerScore) {
+        return "player won the game"
+    } else {
+        return "it's a tie";
+    }
 }
+
+function calculateScore(roundWinnerText) {
+
+    if (roundWinnerText.subString(0,4) === "You w") {
+        return "player";
+    } else if( roundWinnerText.subString(0,4) === "You l") {
+        return "computer";
+    } else {
+        return "tie";
+    }
+}
+
